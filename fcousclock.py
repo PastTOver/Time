7603b224ff8c3227f69d4f45046fcc8e792b7fa2
PastTOver <57899919+PastTOver@users.noreply.github.com>
2023-08-09 12:39:58 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,13 +1,26 @@
-import time
+from datetime import datetime
 
-def focus_timer(minutes):
-    """Function to run the focus timer for a specified number of minutes"""
-    seconds = minutes * 60
-    while seconds > 0:
-        print(f"Time remaining: {seconds//60} minutes {seconds%60} seconds")
-        time.sleep(1)
-        seconds -= 1
-    print("Time is up! Focus session is over.")
+"""Python日期时间到时间戳"""
+# 当前日期和时间
+now = datetime.now()
+timestamp = datetime.timestamp(now)
+print('时间戳：', timestamp)
+print('类型：', type(timestamp))
 
-if __name__ == "__main__":
-    focus_timer(25) # run a 25 minute focus session
+# 10位的时间戳，精确到秒
+ts10 = str(timestamp).split('.')[0]
+print('10位时间戳:', ts10)
+
+# 13位的时间戳，精确到毫秒
+ts13 = str(timestamp*1000).split('.')[0]
+print('13位时间戳:', ts13)
+
+
+"""Python时间戳到日期时间"""
+timestamp = 1675042819.1232566
+dt1 = datetime.fromtimestamp(timestamp)
+print("时间戳转日期：", dt1)
+print("类型：", type(dt1))
+
+dt = datetime.fromtimestamp(int(ts10))
+print("10位时间戳转日期：", dt)
